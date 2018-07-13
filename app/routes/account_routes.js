2cadbcdb004fbbf0ed3ad5ff5e6c6565e7e4a228
alexander.dohrn@forrent.com <alexander.dohrn@forrent.com>
2018-07-13 13:50:53 -0400

    Add better item filtration and optimization

--- app/routes/account_routes.js
+++ app/routes/account_routes.js
@@ -1,8 +1,12 @@
+const bodyParser     = require('body-parser');
+
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
+	app.use(bodyParser.urlencoded({ extended: true }));
+
 	app.get('/accounts/:id', (req, res) => {
-		const id = req.params.id;
+		console.log(req.params.id);
 	    const details = { '_id': req.params.id };
 	    db.collection('accounts').findOne(details, (err, account) => {
 	      if (err) {
@@ -11,5 +15,41 @@ module.exports = function(app, db) {
 	        res.send(account);
 	      }
 	    });
-  });
+  	});
+  	app.get('/items', (req, res) => {
+  		let matchConditions = [];
+  		let shouldProject = 0;
+
+  		// Order in subjected rarity so mongo can filter faster
+  		if (req.query.iMods)
+  			matchConditions.push({ 'stashes.items.implicitMods': {$regex: req.query.iMods, $options: '$i'} });
+
+  		if (req.query.typeLine)
+  			matchConditions.push({ 'stashes.items.typeLine': {$regex: req.query.typeLine, $options: '$i'} });
+
+  		if (req.query.ilvl)
+  			matchConditions.push({ 'stashes.items.ilvl': parseInt(req.query.ilvl) });
+
+  		if (req.query.lvlreq)
+  			matchConditions.push({ 'stashes.items.requirements.name': 'Level' });
+
+  		db.collection('accounts').aggregate([
+  			{
+  				$unwind: '$stashes'
+  			}, 
+  			{
+  				$unwind: '$stashes.items'
+  			},
+  			{
+  				$match: {
+  					$and: matchConditions
+  				}
+  			}
+  		]).toArray().then((result, err) => {
+  			if (err)
+  				res.send({'error': err})
+  			else
+  				res.send(result);
+  		})
+  	});
 };
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express        = require('express');
-const bodyParser     = require('body-parser');
 const app            = express();
 const fs             = require('fs');
 const scrapeHtml     = require('./scrape_html.js');
@@ -10,8 +9,6 @@ const port = 8000;
 
 const API_URL = 'http://api.pathofexile.com/public-stash-tabs/?id=';
 const STARTER_ID = '224521283-233533891-219681636-252343422-237534337';
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -25,13 +22,17 @@ async function main() {
 		console.log('We are live on ' + port);
 	});
 
-	var counter = 0;
+	// PoE api returns same tabId if there aren't any updates. If there are 100 duplicates, we should probably stop.
+	var duplicateCounter = 110;
 	var currTabId = STARTER_ID;
 
-	while (counter++ < 1000) {
+	while (duplicateCounter < 100) {
 		const data = await scrapeHtml.scrape(API_URL + currTabId);
 		const desiredData = processData.process(data);
 		dbHandler.updateAccounts(desiredData.accountsHash);
+
+		if (currTabId == desiredData.nextId)
+			duplicateCounter++;
 
 		currTabId = desiredData.nextId;
 		console.log(desiredData.nextId);
